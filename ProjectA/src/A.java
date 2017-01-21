11cb4bdc01142082181fc09c23448bf7d8d0a06d
valencio <valencio@192.168.1.17>
2017-01-21 19:58:20 +0000

    Squashed commit of the following:
    commit fe3783090f8f983ab118833557631657f5997eb6
    Author: valencio <valencio@192.168.1.17>
    Date:   Sat Jan 21 19:58:14 2017 +0000
    
    	test

--- ProjectA/src/A.java
+++ ProjectA/src/A.java
@@ -2,9 +2,8 @@
 public class A {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("this is a test message");
-System.out.println("after stash");
-	}
 
+	System.out.println(153/10);	
+	System.out.println(153%10);	
+	}
 }
